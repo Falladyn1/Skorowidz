341d212b725125dfc8856b8493a206119fe2495e
Falladyn1 <m.falkowski04@gmail.com>
2025-12-18 17:26:07 +0100

    Refactor index.c for memory management and sorting

--- Skorowidz/index.c
+++ Skorowidz/index.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,90 +10,98 @@ void inicjalizujIndeks(Indeks* indeks) {
 
 void dodajStrone(Slowo* s, int numer) {
     if (s->iloscStron == s->pojemnosc) {
-        s->pojemnosc = s->pojemnosc ? s->pojemnosc * 2 : 4;
-        s->strony = realloc(s->strony, s->pojemnosc * sizeof(int));
+        int nowaPojemnosc = (s->pojemnosc == 0) ? 4 : s->pojemnosc * 2;
+        int* noweStrony = malloc(nowaPojemnosc * sizeof(int));
+
+        if (noweStrony == NULL) exit(1);
+
+        if (s->strony != NULL) {
+            memcpy(noweStrony, s->strony, s->iloscStron * sizeof(int));
+            free(s->strony);
+        }
+
+        s->strony = noweStrony;
+        s->pojemnosc = nowaPojemnosc;
     }
-    s->strony[s->iloscStron++] = numer;
+
+    int i;
+    for (i = 0; i < s->iloscStron; i++) {
+        if (s->strony[i] == numer) return;
+        if (s->strony[i] > numer) break;
+    }
+
+    for (int k = s->iloscStron; k > i; k--) {
+        s->strony[k] = s->strony[k - 1];
+    }
+
+    s->strony[i] = numer;
+    s->iloscStron++;
 }
 
 void dodajSlowo(Indeks* indeks, const char* tekst, int numer) {
-    Slowo* s = indeks->pierwsze;
-    while (s) {
-        if (strcmp(s->tekst, tekst) == 0) {
-            dodajStrone(s, numer);
+    Slowo* aktualny = indeks->pierwsze;
+    Slowo* poprzedni = NULL;
+
+    while (aktualny != NULL) {
+        int porownanie = strcmp(aktualny->tekst, tekst);
+
+        if (porownanie == 0) {
+            dodajStrone(aktualny, numer);
             return;
         }
-        s = s->nast;
-    }
 
-    s = malloc(sizeof(Slowo));
-    s->tekst = _strdup(tekst);
-    s->strony = NULL;
-    s->iloscStron = 0;
-    s->pojemnosc = 0;
-    dodajStrone(s, numer);
-    s->nast = indeks->pierwsze;
-    indeks->pierwsze = s;
-}
-
-void drukujStrony(Slowo* s) {
-    if (s->iloscStron == 0) return;
-
-    int n = s->iloscStron;
-    int* tab = malloc(n * sizeof(int));
-    memcpy(tab, s->strony, n * sizeof(int));
-
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-            if (tab[i] > tab[j]) { int t = tab[i]; tab[i] = tab[j]; tab[j] = t; }
-
-    int start = tab[0], prev = tab[0];
-    for (int i = 1; i < n; i++) {
-        if (tab[i] == prev + 1) prev = tab[i];
-        else {
-            if (start == prev) printf("%d", start);
-            else printf("%d-%d", start, prev);
-            printf(", ");
-            start = prev = tab[i];
+        if (porownanie > 0) {
+            break;
         }
-    }
-    if (start == prev) printf("%d", start);
-    else printf("%d-%d", start, prev);
 
-    free(tab);
+        poprzedni = aktualny;
+        aktualny = aktualny->nast;
+    }
+
+    Slowo* nowe = malloc(sizeof(Slowo));
+    nowe->tekst = malloc(strlen(tekst) + 1);
+    strcpy(nowe->tekst, tekst);
+    nowe->strony = NULL;
+    nowe->iloscStron = 0;
+    nowe->pojemnosc = 0;
+    dodajStrone(nowe, numer);
+
+    if (poprzedni == NULL) {
+        nowe->nast = indeks->pierwsze;
+        indeks->pierwsze = nowe;
+    }
+    else {
+        poprzedni->nast = nowe;
+        nowe->nast = aktualny;
+    }
 }
 
 void wypiszIndeks(const Indeks* indeks) {
-    int count = 0;
-    Slowo* tmp = indeks->pierwsze;
-    while (tmp) { count++; tmp = tmp->nast; }
-
-    Slowo** tab = malloc(count * sizeof(Slowo*));
-    tmp = indeks->pierwsze;
-    for (int i = 0; i < count; i++) { tab[i] = tmp; tmp = tmp->nast; }
-
-    for (int i = 0; i < count - 1; i++)
-        for (int j = i + 1; j < count; j++)
-            if (strcmp(tab[i]->tekst, tab[j]->tekst) > 0) {
-                Slowo* t = tab[i]; tab[i] = tab[j]; tab[j] = t;
-            }
-
-    for (int i = 0; i < count; i++) {
-        printf("%s ", tab[i]->tekst);
-        drukujStrony(tab[i]);
-        printf("\n");
+    Slowo* s = indeks->pierwsze;
+    if (!s) {
+        printf("Indeks pusty.\n");
+        return;
     }
 
-    free(tab);
+    while (s) {
+        printf("%s: ", s->tekst);
+        for (int i = 0; i < s->iloscStron; i++) {
+            printf("%d", s->strony[i]);
+            if (i < s->iloscStron - 1) printf(", ");
+        }
+        printf("\n");
+        s = s->nast;
+    }
 }
 
 void zwolnijIndeks(Indeks* indeks) {
     Slowo* s = indeks->pierwsze;
     while (s) {
-        Slowo* tmpS = s;
+        Slowo* tmp = s;
         s = s->nast;
-        free(tmpS->tekst);
-        free(tmpS->strony);
-        free(tmpS);
+        free(tmp->tekst);
+        free(tmp->strony);
+        free(tmp);
     }
+    indeks->pierwsze = NULL;
 }
